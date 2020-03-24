e6c6298aa76820d07c9bad3c0f9c7520aee4c3ae
wfunnell <wfunnell@homeaway.com>
2020-03-24 22:03:27 +0000

    created greet service

---
+++ src/main/java/grpc/greeting/server/GreetServiceImpl.java
@@ -0,0 +1,28 @@
+package grpc.greeting.server;
+
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
+import io.grpc.stub.StreamObserver;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        Greeting greeting = request.getGreeting();
+        String firstName = greeting.getFirstName();
+
+        String result = "Hello " + firstName;
+
+        GreetResponse response = GreetResponse.newBuilder()
+                .setResult(result)
+                .build();
+
+        // send the response
+        responseObserver.onNext(response);
+
+        //complete the RPC call
+        responseObserver.onCompleted();
+    }
+}
