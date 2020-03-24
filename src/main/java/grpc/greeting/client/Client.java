eda8104460524ef859b66b5f32d29a7fa2e837e0
wfunnell <wfunnell@homeaway.com>
2020-03-24 22:26:36 +0000

    working client server implementation

--- src/main/java/grpc/greeting/client/Client.java
+++ src/main/java/grpc/greeting/client/Client.java
@@ -1,6 +1,10 @@
 package grpc.greeting.client;
 
 import com.proto.dummy.DummyServiceGrpc;
+import com.proto.greet.GreetRequest;
+import com.proto.greet.GreetResponse;
+import com.proto.greet.GreetServiceGrpc;
+import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -10,15 +14,26 @@ public class Client {
         System.out.println("Hello, I am a gRPC client");
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext() //added due to lack of SSL setup
                 .build();
 
         System.out.println("Creating stub");
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
-        //
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
         // DummyServiceGrpc.DummyServiceFutureStub futureStub = DummyServiceGrpc.newFutureStub(channel);
 
-        //do something
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+        Greeting greeting = Greeting.newBuilder()
+                .setFirstName("Will")
+                .setLastName("Funnell")
+                .build();
+
+        GreetRequest request = GreetRequest.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        GreetResponse greetResponse = greetClient.greet(request);
 
         System.out.println("Shutting down channel");
         channel.shutdown();
