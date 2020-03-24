706e89749a277321d93657bfa947d8091efe5eff
wfunnell <wfunnell@homeaway.com>
2020-03-24 21:25:21 +0000

    server setup

---
+++ src/main/java/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,26 @@
+package grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread( () -> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        server.awaitTermination();
+    }
+}
